685706066a744befe46c1e37b2f4f2c28fe32730
Rampeur <rampeur@gmail.com>
2019-08-15 21:13:52 +0200

    Mise à jour des valeurs par defaut de la configuration et compatibilité avec Symfony >= 4.3

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,6 +2,7 @@
 
 namespace Rampeur\Bundle\CookieConsentBundle\DependencyInjection;
 
+use App\Kernel;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -15,31 +16,37 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('rampeur_cookie_consent');
+        if (Kernel::VERSION_ID >= 40200) {
+            $treeBuilder = new TreeBuilder('rampeur_cookie_consent');
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            $treeBuilder = new TreeBuilder();
+            $rootNode = $treeBuilder->root('rampeur_cookie_consent');
+        }
         $rootNode
             ->children()
                 ->arrayNode('layout')
                     ->addDefaultsIfNotSet()
                     ->children()
-                        ->scalarNode('position')->defaultValue('top')->end()
+                        ->scalarNode('position')->defaultValue('bottom-right')->end()
                         ->booleanNode('static')->defaultFalse()->end()
-                        ->scalarNode('theme')->defaultValue('edgeless')->end()
+                        ->scalarNode('theme')->defaultValue('wire')->end()
                         ->arrayNode('palette')
                             ->addDefaultsIfNotSet()
                             ->children()
                                 ->arrayNode('popup')
                                     ->addDefaultsIfNotSet()
                                     ->children()
-                                        ->scalarNode('background')->defaultValue('#252e39')->end()
+                                        ->scalarNode('background')->defaultValue('#333333')->end()
                                         ->scalarNode('text')->defaultValue('#ffffff')->end()
                                     ->end()
                                 ->end()
                                 ->arrayNode('button')
                                     ->addDefaultsIfNotSet()
                                     ->children()
-                                        ->scalarNode('background')->defaultValue('#14a7d0')->end()
-                                        ->scalarNode('text')->defaultValue('#ffffff')->end()
+                                        ->scalarNode('background')->defaultValue('transparent')->end()
+                                        ->scalarNode('text')->defaultValue('#14a7d0')->end()
+                                        ->scalarNode('border')->defaultValue('#14a7d0')->end()
                                     ->end()
                                 ->end()
                             ->end()
